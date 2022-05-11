c781e96492d06000b6438a0384d37b0c5673f0eb
Ernest Leung <ernestleung52@gmail.com>
2022-05-11 11:32:27 -0700

    Updated endorse all

---
+++ playground.js
@@ -0,0 +1,33 @@
+// Brute Force -> TC: O(n^2), SC: O(1)
+
+const twoSum = (array, target) => {
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = i + 1; j < array.length - 1; j += 1) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+// Hash Tabel Approach -> TC: O(n), SC: O(n)
+
+const twoSum2 = (array, target) => {
+  const hashTable = {};
+  for (let i = 0; i < array.length; i += 1) {
+    // if(hashTable[array[i]]) return true;
+    let potentialMatch = target - array[i];
+    // hashTable[potentialMatch] = true;
+    
+    if (potentialMatch in hashTable) {
+        return true;
+    } else {
+        
+        hashTable[array[i]] = potentialMatch;
+    }
+  }
+  return false;
+};
+
+console.log(twoSum2([1, 2, 5, 7, 8], 13));
--- scripts/linkedInEndorseAll.js
+++ scripts/linkedInEndorseAll.js
@@ -21,28 +21,6 @@
 //   }
 // };
 
-// const endorseAll2 = async () => {
-const clickLoadMore = (i) => {
-  setTimeout(() => {
-    const list = document.getElementsByClassName(
-      'artdeco-button artdeco-button--muted artdeco-button--1 artdeco-button--full artdeco-button--secondary ember-view scaffold-finite-scroll__load-button'
-    );
-    console.log(i);
-    list[0].click();
-  }, 2000 * i);
-};
-
-const loadAll = async () =>
-  new Promise((resolve) => {
-    //   if (list[0] !== undefined) {
-    for (let i = 0; i < 3; i += 1) {
-      clickLoadMore(i);
-      // }
-    }
-    resolve();
-  });
-await loadAll();
-
 // const endorseFunction = () => {
 //   const endorseList = document.querySelectorAll('.pvs-list .pv2 button');
 //   const endorse = (i, skill) =>
@@ -57,6 +35,31 @@ await loadAll();
 // endorseFunction();
 // };
 
+// const endorseAll2 = async () => {
+
+// One click to endorse all
+// 1. (Optional) Run loadAll() to expand the list then wait for completed message until running endorseFunc
+const clickLoadMore = (i) => {
+  setTimeout(() => {
+    const list = document.getElementsByClassName(
+      'artdeco-button artdeco-button--muted artdeco-button--1 artdeco-button--full artdeco-button--secondary ember-view scaffold-finite-scroll__load-button'
+    );
+    console.log(i);
+    if (list[0] === undefined) console.log('Completed loading all Skills');
+    list[0].click();
+  }, 2000 * i);
+};
+
+const loadAll = async () =>
+  new Promise((resolve) => {
+    for (let i = 0; i < 3; i += 1) {
+      clickLoadMore(i);
+    }
+    resolve();
+  });
+await loadAll();
+
+// 2. Click all endorse buttons ( scroll to the bottom of skill page and run endorseFunc if not running loadAll() first) * Will un-click buttons if it's already been clicked
 const endorseFunction = () => {
   const endorseList = document.querySelectorAll('.pvs-list .pv2 button');
   for (let i = 0; i < Math.ceil(endorseList.length / 2 + 1); i += 1) {
