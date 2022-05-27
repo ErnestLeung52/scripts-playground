d8f39df7864a0249914069643a2bc79faac87194
Ernest Leung <ernestleung52@gmail.com>
2022-05-26 23:52:39 -0700

    SLL

--- Data Structure/SinglyLinkedList.js
+++ Data Structure/SinglyLinkedList.js
@@ -11,11 +11,11 @@ class Node {
   }
 }
 
-//Searches a value in the given list (Iterative)
+// SEARCHES a value in the given list
+// (Iterative) | TC: O(N) | SC: O(1)
 LinkedList.prototype.search = (value) => {
   //Start from the first element
   let currentNode = this.head;
-
   //Traverse the list until you find the value or reach the end
   while (currentNode != null) {
     if (currentNode.data == value) {
@@ -26,7 +26,22 @@ LinkedList.prototype.search = (value) => {
   return false; //value not found
 };
 
-// (Recursive)
-LinkedList.prototype.searchRecursive = (value) => {
-    
-}
+// (Recursive) | TC: O(N) | SC: O(N)
+LinkedList.prototype.searchRecursive = (node, value) => {
+  if (node !== null) return false;
+  if (node.data === value) return true;
+  return this.searchRecursive(node.nextElement, value);
+};
+
+// DELETE a value
+LinkedList.prototype.delete = (value) => {
+  //if list is empty, do nothing
+  if (this.isEmpty()) {
+    return this;
+  }
+  //Get the head and first element of the list
+  let firstElement = this.head;
+  //If list is not empty, link head to the nextElement of firstElement
+  this.head = firstElement.nextElement;
+  return this;
+};
