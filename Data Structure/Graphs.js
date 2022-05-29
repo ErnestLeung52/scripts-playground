36d78a11710e0469ec26b5a41015e8898ff1e66f
Ernest Leung <ernestleung52@gmail.com>
2022-05-29 14:11:24 -0700

    Graph added edge

--- Data Structure/Graphs.js
+++ Data Structure/Graphs.js
@@ -1,13 +1,22 @@
 class Graph {
-    constructor(vertices) {
-        // Total number of vertices in the graph
-        this.vertices = vertices;
-        // Defined an array which can hold LL equal to the number of vertices in the graph
-        this.list = [];
-        // Creating a new LL for each vertex/index of the list
-        for (i = 0; i < vertices.length; i++) {
-            let temp = new LinkedList();
-            this.list.push(temp);
-        }
+  constructor(vertices) {
+    // Total number of vertices in the graph
+    this.vertices = vertices;
+    // Defined an array which can hold LL equal to the number of vertices in the graph
+    this.list = [];
+    // Creating a new LL for each vertex/index of the list
+    for (let i = 0; i < vertices.length; i++) {
+      let temp = new LinkedList();
+      this.list.push(temp);
     }
+  }
+
+  addEdge(source, destination) {
+    if (source < this.vertices && destination < this.vertices)
+      //Since we are implementing a directed list, (0,1) is not the same as (1,0)
+      this.list[source].insertAtHead(destination);
+    //If we were to implement an undirected graph where (0,1)==(1,0),
+    //we would create an additional edge from destination to source too:
+    //this.list[destination].insertAtHead(source);
+  }
 }
