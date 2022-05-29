2c32203df6cd4a014a94025a48591f57fb8f233a
Ernest Leung <ernestleung52@gmail.com>
2022-05-28 22:02:24 -0700

    added second method of find nth

--- Data Structure/SinglyLinkedList.js
+++ Data Structure/SinglyLinkedList.js
@@ -300,7 +300,31 @@ LinkedList.prototype.findNth = function (list, n) {
   return nthNode;
 };
 
-LinkedList.prototype.findNth2 = function (list, n)
+LinkedList.prototype.findNth2 = function (list, n) {
+  let nthNode = list.getHead(); //This iterator will reach the nth node
+  let endNode = list.getHead(); //This iterator will reach the end
+
+  let count = 0;
+  if (!list.isEmpty()) {
+    while (count < n) {
+      if (endNode == null) {
+        console.log('Out of bounds');
+        return null;
+      }
+      endNode = endNode.nextElement;
+      count++;
+    }
+  }
+  while (endNode != null) {
+    endNode = endNode.nextElement;
+    nthNode = nthNode.nextElement;
+  }
+  if (nthNode != null) {
+    return nthNode;
+  } else {
+    return null;
+  }
+};
 
 // dummy
 let list = new LinkedList();
