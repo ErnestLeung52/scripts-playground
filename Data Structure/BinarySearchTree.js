c2fa0d3f7fdbf42172eb90a7423d93f458b6ed4a
Ernest Leung <ernestleung52@gmail.com>
2022-05-30 16:38:34 -0700

    BST insert

--- Data Structure/BinarySearchTree.js
+++ Data Structure/BinarySearchTree.js
@@ -19,3 +19,35 @@ class BinarySearchTree {
 
 // const BST= new BinarySearchTree(8);
 
+BinarySearchTree.prototype.insert = function (newValue) {
+  if (this.root === null) {
+    this.root = new Node(newValue);
+    return;
+  }
+
+  // Starting from the root
+  let currentNode = this.root;
+  let parent;
+  // while we get to the null node
+  while (currentNode) {
+    parent = currentNode; //update the parent
+    if (newValue < currentNode.val) {
+      //if newValue < currentNode.val, iterate to the left subtree
+      currentNode = currentNode.leftChild;
+    } else {
+      //if newValue >= currentNode.val, iterate to the right subtree
+      currentNode = currentNode.rightChild;
+    }
+  }
+  //by now, we will have the parent of the null
+  //node where we have to insert the newValue
+  if (newValue < parent.val) {
+    //if newValue < parent.val, insert into the leftChild
+    parent.leftChild = new Node(newValue);
+  } else {
+    //if newValue >= parent.val, insert into the rightChild
+    parent.rightChild = new Node(newValue);
+  }
+};
+
+// const bsTree= new BinarySearchTree(8);
