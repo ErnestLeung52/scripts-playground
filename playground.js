5979e1e9dc726530601221158bd8dbbdf83c860c
Ernest Leung <ernestleung52@gmail.com>
2022-06-08 17:42:59 -0700

    playground

--- playground.js
+++ playground.js
@@ -1,13 +1,23 @@
-var goodNodes = function(root) {
-  let result = 0;
-  function helper(node, prev) {
-      if (node === undefined || node === null) return;
-      if (node.val >= prev) {
-      result += 1;
-      }
-  
-  prev = Math.max(node.val, prev)
-  }
+var goodNodes = function (root) {
+	let result = 0;
+	function helper(node, prev) {
+		if (node === undefined || node === null) return;
+		if (node.val >= prev) {
+			result += 1;
+		}
+
+		prev = Math.max(node.val, prev);
+	}
 };
 
-
+function nbYear(p0, percent, aug, p) {
+	let years = 1;
+	let inhabitants = p0 + p0 * (percent / 100) + aug;
+	while (inhabitants < p) {
+		inhabitants = inhabitants + inhabitants * (percent / 100) + aug;
+		years += 1;
+	}
+  console.log(inhabitants);
+	return years;
+}
+console.log(nbYear(1000, 2, 50, 1214));
