007135bf0c56e546a429ed2298a241c41d3f790f
Ernest Leung <ernestleung52@gmail.com>
2022-06-09 17:16:45 -0700

    2pointers P9

--- Algo + DS/Algorithms/TwoPointers.js
+++ Algo + DS/Algorithms/TwoPointers.js
@@ -299,3 +299,43 @@ function dutch_flag_sort(arr) {
 	return arr;
 }
 // console.log(dutch_flag_sort([1, 0, 2, 1, 0]));
+
+/* -------------- 9. Quadruple Sum to Target  ------------------
+Given an array of unsorted numbers and a target number, find all unique quadruplets in it, whose sum is equal to the target number.
+Input: [4, 1, 2, -1, 1, -3], target=1
+Output: [-3, -1, 1, 4], [-3, 1, 1, 2]
+*/
+const fourSum = function (nums, target) {
+	nums.sort((a, b) => a - b);
+	const result = [];
+
+  // fix the first number
+	for (let i = 0; i < nums.length - 3; i++) {
+    // fix the second number
+		for (let j = i + 1; j < nums.length - 2; j++) {
+			let low = j + 1;
+			let high = nums.length - 1;
+
+			while (low < high) {
+				const sum = nums[i] + nums[j] + nums[low] + nums[high];
+
+				if (sum === target) {
+					result.push([nums[i], nums[j], nums[low], nums[high]]);
+					while (nums[low] === nums[low + 1]) low++;
+					while (nums[high] === nums[high - 1]) high--;
+					low++;
+					high--;
+				} else if (sum < target) {
+					low++;
+				} else {
+					high--;
+				}
+			}
+			while (nums[j] === nums[j + 1]) j++;
+		}
+		while (nums[i] === nums[i + 1]) i++;
+	}
+	return result;
+};
+// console.log(fourSum([4, 1, 2, -1, 1, -3], 1));
+// [-3, -1, 1, 1, 2, 4]
