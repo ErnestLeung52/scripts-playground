7972cffd7d56d016af4a0802fbe140bf2e0c9aab
Ernest Leung <ernestleung52@gmail.com>
2022-06-10 18:28:05 -0700

    slow fast P6

--- Algo + DS/Algorithms/fastAndSlowPointers.js
+++ Algo + DS/Algorithms/fastAndSlowPointers.js
@@ -141,7 +141,7 @@ const is_palindromic_linked_list = function (head) {
 	let copyHeadSecondHalf = headSecondHalf;
 
 	// The reverse function will unlink first half / second half, since midpoint is now point to null
-    // Then we can compare both
+	// Then we can compare both
 
 	while (head !== null && headSecondHalf !== null) {
 		if (head.value !== headSecondHalf.value) {
@@ -168,3 +168,58 @@ function reverseLL(head) {
 	}
 	return prev;
 }
+
+/* -------------- 6. Rearrange a LinkedList ------------------
+Given the head of a Singly LinkedList, write a method to modify the LinkedList such that the nodes from the second half of the LinkedList are inserted alternately to the nodes from the first half in reverse order. So if the LinkedList has nodes 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null, your method should return 1 -> 6 -> 2 -> 5 -> 3 -> 4 -> null.
+*/
+function reorder(head) {
+	if (head === null || head.next === null) {
+		return;
+	}
+
+	// find middle of the LinkedList
+	let slow = head,
+		fast = head;
+	while (fast !== null && fast.next !== null) {
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+
+	// slow is now pointing to the middle node
+	headSecondHalf = reverse(slow); // reverse the second half
+	headFirstHalf = head;
+
+	//2 -> 4 -> 6 -> 8    12 -> 10 -> 8
+	// rearrange to produce the LinkedList in the required order
+	while (headFirstHalf !== null && headSecondHalf !== null) {
+		temp = headFirstHalf.next;
+		headFirstHalf.next = headSecondHalf;
+		headFirstHalf = temp;
+
+		temp = headSecondHalf.next;
+		headSecondHalf.next = headFirstHalf;
+		headSecondHalf = temp;
+	}
+	// set the next of the last node to 'null'
+	if (headFirstHalf !== null) {
+		headFirstHalf.next = null;
+	}
+}
+function reverse(head) {
+	let prev = null,
+		next = null;
+	while (head !== null) {
+		next = head.next;
+		head.next = prev;
+		prev = head;
+		head = next;
+	}
+	return prev;
+}
+var head = new Node(2);
+head.next = new Node(4);
+head.next.next = new Node(6);
+head.next.next.next = new Node(8);
+head.next.next.next.next = new Node(10);
+head.next.next.next.next.next = new Node(12);
+reorder(head);
