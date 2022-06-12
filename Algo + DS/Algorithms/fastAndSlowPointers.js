71c0b1015fcc879c21f9ba76038f89b1d099909b
Ernest Leung <ernestleung52@gmail.com>
2022-06-11 21:07:10 -0700

    fast slow P7

--- Algo + DS/Algorithms/fastAndSlowPointers.js
+++ Algo + DS/Algorithms/fastAndSlowPointers.js
@@ -219,4 +219,58 @@ function reverse(head) {
 
 /* -------------- 7. Cycle in a Circular Array ------------------
 We are given an array containing positive and negative numbers. Suppose the array contains a number ‘M’ at a particular index. Now, if ‘M’ is positive we will move forward ‘M’ indices and if ‘M’ is negative move backwards ‘M’ indices.
+Input: [1, 2, -1, 2, 2]
+Output: true
+Explanation: The array has a cycle among indices: 0 -> 1 -> 3 -> 0
 */
+function circular_array_loop_exists(arr) {
+	for (i = 0; i < arr.length; i++) {
+		isForward = arr[i] >= 0; // if we are moving forward or not
+		let slow = i,
+			fast = i;
+
+		// if slow or fast becomes '-1' this means we can't find cycle for this number
+		while (true) {
+			// move one step for slow pointer
+			slow = find_next_index(arr, isForward, slow);
+			// move one step for fast pointer
+			fast = find_next_index(arr, isForward, fast);
+			if (fast !== -1) {
+				// move another step for the fast pointer
+				fast = find_next_index(arr, isForward, fast);
+			}
+			if (slow === -1 || fast === -1 || slow === fast) {
+				break;
+			}
+		}
+
+		if (slow !== -1 && slow === fast) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function find_next_index(arr, isForward, currentIndex) {
+	direction = arr[currentIndex] >= 0;
+
+	if (isForward !== direction) {
+		return -1; // change in direction, return -1
+	}
+
+	nextIndex = (currentIndex + arr[currentIndex]) % arr.length;
+    currentIndex
+	console.log(nextIndex);
+	if (nextIndex < 0) {
+		nextIndex += arr.length; // wrap around for negative numbers
+	}
+
+	// one element cycle, return -1
+	if (nextIndex === currentIndex) {
+		nextIndex = -1;
+	}
+
+	return nextIndex;
+}
+// console.log(circular_array_loop_exists([1, 2, -1, 2, 2]));
