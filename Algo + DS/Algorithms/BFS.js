3d0724375cbe65975e62627077f79624f71788e6
Ernest Leung <ernestleung52@gmail.com>
2022-06-18 16:37:30 -0700

    BFS P4

--- Algo + DS/Algorithms/BFS.js
+++ Algo + DS/Algorithms/BFS.js
@@ -8,6 +8,9 @@ class TreeNode {
 
 /*------------------- 1. Binary Tree Level Order Traversal ---------------------
 Given a binary tree, populate an array to represent its level-by-level traversal. You should populate the values of all nodes of each level from left to right in separate sub-arrays.
+        1
+      2   3
+    4  5 6  7
 */
 const BFS = function (root) {
 	result = [];
@@ -41,6 +44,9 @@ const BFS = function (root) {
 
 /*------------------- 2. Reverse Level Order Traversal ---------------------
 Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, i.e., the lowest level comes first. You should populate the values of all nodes in each level from left to right in separate sub-arrays.
+        1
+      2   3
+    4  5 6  7
 */
 const BFSreverse = function (root) {
 	const result = [];
@@ -99,10 +105,37 @@ const zigzag = function (root) {
 				queue.push(currentNode.right);
 			}
 		}
-
 		result.push(level);
 		leftToRight = !leftToRight;
 	}
 	return result;
 };
 
+/*------------------- 4. Level Averages in a Binary Tree  ---------------------
+Given a binary tree, populate an array to represent the averages of all of its levels.
+*/
+const find_level_averages = function (root) {
+	const result = [];
+	const queue = [];
+	queue.push(root);
+
+	while (queue.length > 0) {
+        // Need to fix levelSize because queue is constantly changing
+		const levelSize = queue.length;
+		let sum = 0;
+
+		for (let i = 0; i < levelSize; i++) {
+			const currentNode = queue.shift();
+			sum += currentNode.val;
+
+			if (currentNode.left) {
+				queue.push(currentNode.left);
+			}
+			if (currentNode.right) {
+				queue.push(currentNode.right);
+			}
+		}
+		result.push(sum / levelSize);
+	}
+    return result;
+};
