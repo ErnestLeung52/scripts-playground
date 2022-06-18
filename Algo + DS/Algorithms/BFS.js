edc2d7c573ac8f338ea9f8aa980465932bb520be
Ernest Leung <ernestleung52@gmail.com>
2022-06-17 21:56:31 -0700

    BFS P2

--- Algo + DS/Algorithms/BFS.js
+++ Algo + DS/Algorithms/BFS.js
@@ -39,7 +39,7 @@ const BFS = function (root) {
 	return result;
 };
 
-/*------------------- 1. Reverse Level Order Traversal ---------------------
+/*------------------- 2. Reverse Level Order Traversal ---------------------
 Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, i.e., the lowest level comes first. You should populate the values of all nodes in each level from left to right in separate sub-arrays.
 */
 const BFSreverse = function (root) {
