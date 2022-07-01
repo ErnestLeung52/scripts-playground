7a4f6e63bb88e66bcffb5cd31d277cd0c8d6991e
Ernest Leung <ernestleung52@gmail.com>
2022-06-30 17:44:18 -0700

    subset P1

--- Algo + DS/Algorithms/Subsets.js
+++ Algo + DS/Algorithms/Subsets.js
@@ -1,20 +1,23 @@
-/*------------------- 1. Subsets  ---------------------
+/*------------------- P1. Subsets  ---------------------
 Given a set with distinct elements, find all of its distinct subsets.
 */
 const find_subsets = function (nums) {
 	const subsets = [];
-
+	// start by adding the empty subset
 	subsets.push([]);
-
-	for (let i = 0; i < nums.length; i++) {
-		let currentNumber = nums[i];
+	for (i = 0; i < nums.length; i++) {
+		currentNumber = nums[i];
+		// we will take all existing subsets and insert the current number in them to create new subsets
 		const n = subsets.length;
-
-		for (let j = 0; j < n; j++) {
-			const set1 = subsets[j].slice(0);
+		subsets;
+		for (j = 0; j < n; j++) {
+			// create a new subset from the existing subset and insert the current element to it
+			const set1 = subsets[j].slice(); // clone the permutation
 			set1.push(currentNumber);
 			subsets.push(set1);
 		}
 	}
+
 	return subsets;
 };
+// console.log(find_subsets([1, 5, 3]));
