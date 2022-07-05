f6dd9fa01b6a20b2289d4e6166b7e432d3d1f7a5
Ernest Leung <ernestleung52@gmail.com>
2022-07-05 15:30:33 -0700

    subset p1

--- Algo + DS/Algorithms/Subsets.js
+++ Algo + DS/Algorithms/Subsets.js
@@ -5,10 +5,12 @@ const find_subsets = function (nums) {
 	const subsets = [];
 	// start by adding the empty subset
 	subsets.push([]);
+    // 1st loop: iterate through nums
 	for (i = 0; i < nums.length; i++) {
 		currentNumber = nums[i];
 		// we will take all existing subsets and insert the current number in them to create new subsets
 		const n = subsets.length;
+        // 2nd loop: iterate through all the combination in subsets so we can add a new element to each of subArr
 		for (j = 0; j < n; j++) {
 			// create a new subset from the existing subset and insert the current element to it
 			const set1 = subsets[j].slice(); // clone the permutation
@@ -273,3 +275,50 @@ function generate_generalized_abbreviation(word) {
 	}
 	return result;
 }
+
+/*------------------- P7. Evaluate Expression ---------------------
+Given an expression containing digits and operations (+, -, *), find all possible ways in which the expression can be evaluated by grouping the numbers and operators using parentheses.
+*/
+
+/*------------------- P8. Unique Binary Search Trees ---------------------
+Given a number ‘n’, write a function to return all structurally unique Binary Search Trees (BST) that can store values 1 to ‘n’?
+*/
+class TreeNode {
+	constructor(val, left = null, right = null) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+function find_unique_trees(n) {
+    if (n <= 0) {
+      return [];
+    }
+    return findUnique_trees_recursive(1, n);
+  }
+function findUnique_trees_recursive(start, end) {
+	const result = [];
+	// base condition, return 'null' for an empty sub-tree
+	// consider n = 1, in this case we will have start = end = 1, this means we should have only one tree
+	// we will have two recursive calls, findUniqueTreesRecursive(1, 0) & (2, 1)
+	// both of these should return 'null' for the left and the right child
+	if (start > end) {
+		result.push(null);
+		return result;
+	}
+
+	for (let i = start; i < end + 1; i++) {
+		// making 'i' the root of the tree
+		const leftSubtrees = findUnique_trees_recursive(start, i - 1);
+		const rightSubtrees = findUnique_trees_recursive(i + 1, end);
+		for (let p = 0; p < leftSubtrees.length; p++) {
+			for (let q = 0; q < rightSubtrees.length; q++) {
+				const root = new TreeNode(i, leftSubtrees[p], rightSubtrees[q]);
+				result.push(root);
+			}
+		}
+	}
+
+	return result;
+}
+// console.log(find_unique_trees(3));
