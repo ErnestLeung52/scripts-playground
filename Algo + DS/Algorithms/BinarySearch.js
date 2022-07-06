da7cde7b3b4266d1c716abd7b40d427d356746d6
Ernest Leung <ernestleung52@gmail.com>
2022-07-06 14:42:16 -0700

    binary search p9.2

--- Algo + DS/Algorithms/BinarySearch.js
+++ Algo + DS/Algorithms/BinarySearch.js
@@ -314,3 +314,44 @@ function search_rotated_array(arr, key) {
 	// we are not able to find the element in the given array
 	return -1;
 }
+
+/* -------------- 9.2 Search in Rotated Array  ------------------
+How do we search in a sorted and rotated array that also has duplicates?
+Input: [3, 7, 3, 3, 3], key = 7
+Output: 1
+*/
+function search_rotated_with_duplicates(arr, key) {
+	let start = 0;
+	let end = arr.length - 1;
+	while (start <= end) {
+		mid = Math.floor(start + (end - start) / 2);
+		if (arr[mid] === key) {
+			return mid;
+		}
+
+		// the only difference from the previous solution,
+		// if numbers at indexes start, mid, and end are same, we can't choose a side
+		// the best we can do, is to skip one number from both ends as key !== arr[mid]
+		if (arr[start] === arr[mid] && arr[end] === arr[mid]) {
+			start += 1;
+			end -= 1;
+		} else if (arr[start] <= arr[mid]) {
+			// left side is sorted in ascending order
+			if (key >= arr[start] && key < arr[mid]) {
+				end = mid - 1;
+			} else {
+				// key > arr[mid]
+				start = mid + 1;
+			}
+		} else {
+			// right side is sorted in ascending order
+			if (key > arr[mid] && key <= arr[end]) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+	}
+	// we are not able to find the element in the given array
+	return -1;
+}
