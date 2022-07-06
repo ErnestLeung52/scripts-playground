7850daa1093e12ab00f274d330c038211bf279bc
Ernest Leung <ernestleung52@gmail.com>
2022-07-06 00:24:06 -0700

    bs p4

--- Algo + DS/Algorithms/BinarySearch.js
+++ Algo + DS/Algorithms/BinarySearch.js
@@ -90,3 +90,8 @@ function search_next_letter(letters, key) {
 	// since the loop is running until 'start <= end', so at the end of the while loop, 'start === end+1'
 	return letters[start % letters.length];
 }
+
+/* -------------- 4. Number Range ------------------
+Given an array of numbers sorted in ascending order, find the range of a given number ‘key’. The range of the ‘key’ will be the first and last position of the ‘key’ in the array.
+Write a function to return the range of the ‘key’. If the ‘key’ is not present return [-1, -1].
+*/
