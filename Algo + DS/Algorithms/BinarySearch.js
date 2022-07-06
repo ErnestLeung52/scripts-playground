bd08edaebf69ad0fdcaed7c569245b9c4b514b4a
Ernest Leung <ernestleung52@gmail.com>
2022-07-05 23:44:10 -0700

    binary search P2

--- Algo + DS/Algorithms/BinarySearch.js
+++ Algo + DS/Algorithms/BinarySearch.js
@@ -11,27 +11,52 @@ const binary_search = function (arr, key) {
 	while (start <= end) {
 		let mid = Math.floor((start + end) / 2);
 
-        if (arr[mid] === key) {
-            return mid;
-        }
-        if (isAscending) {
-            if (arr[mid] < key) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        } else {
-            if (arr[mid] < key) {
-                end = mid - 1
-            } else {
-                start = mid + 1;
-            }
-        }
+		if (arr[mid] === key) {
+			return mid;
+		}
+		if (isAscending) {
+			if (arr[mid] < key) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		} else {
+			if (arr[mid] < key) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
 	}
-    return - 1;
+	return -1;
 };
 
 /* -------------- 2. Ceiling of a Number ------------------
 Given an array of numbers sorted in an ascending order, find the ceiling of a given number ‘key’. The ceiling of the ‘key’ will be the smallest element in the given array greater than or equal to the ‘key’.
 Write a function to return the index of the ceiling of the ‘key’. If there isn’t any ceiling return -1.
+Input: [1, 3, 8, 10, 15, 16, 17], key = 14
+Output: 4
 */
+function search_ceiling_of_a_number(arr, key) {
+	if (key > arr[arr.length - 1]) {
+		return -1;
+	}
+
+	let start = 0,
+		end = arr.length - 1;
+
+	while (start <= end) {
+		let mid = Math.floor((start + end) / 2);
+
+		if (arr[mid] === key) {
+			return mid;
+		}
+
+		if (key < arr[mid]) {
+			end = mid - 1;
+		} else {
+			start = mid + 1;
+		}
+	}
+	return start;
+}
