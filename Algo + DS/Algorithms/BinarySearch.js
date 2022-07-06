a3098997ed0bf874ab2b10388d285aa73a0428cd
Ernest Leung <ernestleung52@gmail.com>
2022-07-06 01:18:23 -0700

    binary search p4

--- Algo + DS/Algorithms/BinarySearch.js
+++ Algo + DS/Algorithms/BinarySearch.js
@@ -91,7 +91,44 @@ function search_next_letter(letters, key) {
 	return letters[start % letters.length];
 }
 
-/* -------------- 4. Number Range ------------------
+/* -------------- 4. Number Range ------------------ leetcode 34
 Given an array of numbers sorted in ascending order, find the range of a given number ‘key’. The range of the ‘key’ will be the first and last position of the ‘key’ in the array.
 Write a function to return the range of the ‘key’. If the ‘key’ is not present return [-1, -1].
+Input: [4, 6, 6, 6, 9], key = 6
+Output: [1, 3]
 */
+function find_range(arr, key) {
+	const result = [-1, -1];
+	result[0] = binary_search_range(arr, key, false);
+	if (result[0] !== -1) {
+		// no need to search, if 'key' is not present in the input array
+		result[1] = binary_search_range(arr, key, true);
+	}
+
+	return result;
+}
+
+// modified Binary Search
+function binary_search_range(arr, key, findMaxIndex) {
+	let keyIndex = -1;
+	let start = 0;
+	let end = arr.length - 1;
+	while (start <= end) {
+		mid = Math.floor(start + (end - start) / 2);
+		if (key < arr[mid]) {
+			end = mid - 1;
+		} else if (key > arr[mid]) {
+			start = mid + 1;
+		} else {
+			// key === arr[mid]
+			keyIndex = mid;
+			if (findMaxIndex) {
+				start = mid + 1; // search ahead to find the last index of 'key'
+			} else {
+				end = mid - 1; // search behind to find the first index of 'key'
+			}
+		}
+	}
+	return keyIndex;
+}
+// console.log(find_range([4, 6, 6, 6, 9], 6));
