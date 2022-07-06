5e4518ba9f317e40fc93970f8f329d251e36c4b5
Ernest Leung <ernestleung52@gmail.com>
2022-07-06 01:40:31 -0700

    binary search p5

--- Algo + DS/Algorithms/BinarySearch.js
+++ Algo + DS/Algorithms/BinarySearch.js
@@ -132,3 +132,38 @@ function binary_search_range(arr, key, findMaxIndex) {
 	return keyIndex;
 }
 // console.log(find_range([4, 6, 6, 6, 9], 6));
+
+/* -------------- 6. Search in a Sorted Infinite Array ------------------
+Given an infinite sorted array (or an array with unknown size), find if a given number ‘key’ is present in the array. Write a function to return the index of the ‘key’ if it is present in the array, otherwise return -1.
+
+Since it is not possible to define an array with infinite (unknown) size, you will be provided with an interface ArrayReader to read elements of the array. ArrayReader.get(index) will return the number at index; if the array’s size is smaller than the index, it will return Integer.MAX_VALUE.
+*/
+
+function search_in_infinite_array(arr, key) {
+	// find the proper bounds first
+	let start = 0,
+		end = 1;
+	while (arr[end] < key) {
+		let newStart = end + 1;
+		// increase to double the bounds size; (end- start + 1) -> range of start to end
+		end += (end - start + 1) * 2;
+		start = newStart;
+	}
+	return binary_search_infinite(arr, key, start, end);
+}
+
+function binary_search_infinite(arr, key, start, end) {
+	while (start <= end) {
+		mid = Math.floor(start + (end - start) / 2);
+
+		if (key < arr[mid]) {
+			end = mid - 1;
+		} else if (key > arr[mid]) {
+			start = mid + 1;
+		} else {
+			return mid;
+		}
+	}
+	return -1;
+}
+// console.log(search_in_infinite_array([4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30], 10));
