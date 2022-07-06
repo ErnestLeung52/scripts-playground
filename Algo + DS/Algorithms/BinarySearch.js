54ca1f3dd35bfd13bb4b5f102b63833b4a4290d0
Ernest Leung <ernestleung52@gmail.com>
2022-07-06 15:06:45 -0700

    binary search p10

--- Algo + DS/Algorithms/BinarySearch.js
+++ Algo + DS/Algorithms/BinarySearch.js
@@ -355,3 +355,36 @@ function search_rotated_with_duplicates(arr, key) {
 	// we are not able to find the element in the given array
 	return -1;
 }
+
+/* -------------- 10. Rotation Count  ------------------
+Given an array of numbers which is sorted in ascending order and is rotated ‘k’ times around a pivot, find ‘k’.
+You can assume that the array does not have any duplicates.
+Input: [4, 5, 7, 9, 10, 11, 12, -1, 2, 3]
+Output: 5
+*/
+function count_rotations(arr) {
+	let start = 0;
+	let end = arr.length - 1;
+	while (start < end) {
+		mid = Math.floor(start + (end - start) / 2);
+
+		// if mid is greater than the next element
+		if (mid < end && arr[mid] > arr[mid + 1]) {
+			return mid + 1;
+		}
+
+		// if mid is smaller than the previous element
+		if (mid > start && arr[mid] < arr[mid - 1]) {
+			return mid;
+		}
+
+		if (arr[start] < arr[mid]) {
+			// left side is sorted, so the pivot is on right side
+			start = mid + 1;
+		} else {
+			// right side is sorted, so the pivot is on the left side
+			end = mid - 1;
+		}
+	}
+	return 0; // the array has not been rotated
+}
