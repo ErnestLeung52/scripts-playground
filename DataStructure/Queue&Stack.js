22af8ceb268980908a9b5462cd11e7988b24288b
Ernest Leung <ernestleung52@gmail.com>
2022-07-11 00:46:52 -0700

    queue

--- DataStructure/Queue&Stack.js
+++ DataStructure/Queue&Stack.js
@@ -24,3 +24,30 @@ const stack = new Stack();
 // stack.push(3)
 // stack.pop()
 // stack
+
+class Queue {
+	constructor() {
+		this.storage = {};
+		this.index = 0;
+	}
+
+	enqueue(val) {
+		this.storage[this.index++] = value;
+	}
+
+	dequeue() {
+		if (this.index <= 0) return undefined;
+
+		const dequeuedItem = this.storage[0];
+		delete this.storage[0];
+
+        // create 0
+		for (let key in this.storage) {
+			this.storage[key - 1] - this.storage[key];
+		}
+        // delete last
+		delete this.storage[this.index - 1];
+		this.index--;
+		return dequeuedItem;
+	}
+}
