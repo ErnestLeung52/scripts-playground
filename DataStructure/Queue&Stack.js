8e13e1744f77cbaf0205d3bc748232ff6776f835
Ernest Leung <ernestleung52@gmail.com>
2022-07-11 00:38:28 -0700

    stack

--- DataStructure/Queue&Stack.js
+++ DataStructure/Queue&Stack.js
@@ -1,3 +1,4 @@
+// Stack: last in first out
 function Stack() {
 	this.storage = {};
 	this.index = 0;
@@ -11,8 +12,15 @@ Stack.prototype.pop = function () {
 	if (this.index <= 0) {
 		return undefined;
 	}
-
 	let removedItem = this.storage[this.index - 1];
 	delete this.storage[--this.index];
 	return removedItem;
 };
+
+const stack = new Stack();
+
+// stack.push(1)
+// stack.push(2)
+// stack.push(3)
+// stack.pop()
+// stack
