337400ddb33e814e0ad958d743d5a84fef93c453
Ernest Leung <ernestleung52@gmail.com>
2022-07-19 15:21:13 -0700

    TS:

---
+++ TechStack/TypsScript.ts
@@ -0,0 +1,16 @@
+/*
+TypeScript
+1. Help us catch errors during development
+2. Use 'type annotations' to analyze our code
+3. Only active during development
+4. Doesn't provide any performance optimization
+
+TypeScript code -> TS compiler -> Plain old JavaScript
+
+- Install
+npm install -g typescript ts-node
+tsc --help
+
+
+*/
+
