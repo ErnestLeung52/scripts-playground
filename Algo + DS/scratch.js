42068ac55858fd011e2099ce4a56029c1c3587d6
Ernest Leung <ernestleung52@gmail.com>
2023-04-03 21:09:20 -0700

    update

--- Algo + DS/scratch.js
+++ Algo + DS/scratch.js
@@ -343,3 +343,24 @@ const isBalanced = (root) => {
 	// dfs(tree);
 	// return isBalanced;
 };
+
+/* 141. Linked List Cycle
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+*/
+
+var hasCycle = function (head) {
+	let fastPointer = head;
+
+	while (fastPointer && fastPointer.next) {
+		head = head.next;
+		fastPointer = fastPointer.next.next;
+		if (head === fastPointer) {
+			return true;
+		}
+	}
+	return false;
+};
