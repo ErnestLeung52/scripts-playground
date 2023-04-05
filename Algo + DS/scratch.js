90fde2af6a4f4c5114ee87bf60cb62fbcf4351e0
Ernest Leung <ernestleung52@gmail.com>
2023-04-05 15:16:56 -0700

    update

--- Algo + DS/scratch.js
+++ Algo + DS/scratch.js
@@ -363,3 +363,29 @@ var hasCycle = function (head) {
 	}
 	return false;
 };
+
+/* 278. First Bad Version
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+Input: n = 5, bad = 4
+Output: 4
+*/
+var solution = function (isBadVersion) {
+	return function (n) {
+		let low = 1;
+		let high = n;
+		let mid;
+		while (low <= high) {
+			mid = ~~(low + (high - low) / 2);
+			if (isBadVersion(mid)) {
+				if (isBadVersion(mid - 1)) high = mid - 1;
+				else return mid;
+			} else low = mid + 1;
+		}
+	};
+};
+
+
