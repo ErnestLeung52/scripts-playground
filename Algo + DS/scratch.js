8617460f70d8b301eb2d517ddefa58d0a11f556d
Ernest Leung <ernestleung52@gmail.com>
2023-04-13 19:14:20 -0700

    update

--- Algo + DS/scratch.js
+++ Algo + DS/scratch.js
@@ -595,11 +595,11 @@ const maxDepth = (root) => {
 	return Math.max(leftDepth, rightDepth) + 1;
 };
 
-const tree = new TreeNode(1);
-tree.left = new TreeNode(2);
-tree.right = new TreeNode(3);
-tree.right.left = new TreeNode(4);
-tree.right.right = new TreeNode(5);
+// const tree = new TreeNode(1);
+// tree.left = new TreeNode(2);
+// tree.right = new TreeNode(3);
+// tree.right.left = new TreeNode(4);
+// tree.right.right = new TreeNode(5);
 // console.log(maxDepth(tree));
 
 /* 217. Contains Duplicate
@@ -640,4 +640,43 @@ const maxSubArray = (nums) => {
 	}
 
 	return maxSum;
+};
+
+/* 57. Insert Interval
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+*/
+
+const insert = (intervals, newInterval) => {
+	let [newStart, newEnd] = newInterval;
+	const left = [];
+	const right = [];
+
+	for (const interval of intervals) {
+		const [start, end] = interval;
+
+		// current interval is smaller than newInterval
+		if (end < newStart) {
+			left.push(interval);
+		}
+
+		// / current interval is larger than newInterval
+		else if (start > newEnd) {
+			right.push(interval);
+		}
+
+		// there is a overlap
+		else {
+			newStart = Math.min(newStart, start);
+			newEnd = Math.max(newEnd, end);
+		}
+	}
+
+	return [...left, [newStart, newEnd], ...right];
 };
