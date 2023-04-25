088818549392a0ae9e31fe5f7588aa2ed748c1f7
Ernest Leung <ernestleung52@gmail.com>
2023-04-24 18:06:51 -0700

    update

--- Algo + DS/scratch.js
+++ Algo + DS/scratch.js
@@ -1182,3 +1182,11 @@ const coinChange = (coins, amount) => {
 
 	return dp[amount] === Infinity ? -1 : dp[amount];
 };
+
+/* 238. Product of Array Except Self
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+*/
