f19d80ad8e9cd32e69563db75bddcd32f235c6bc
Ernest Leung <ernestleung52@gmail.com>
2023-04-26 12:07:23 -0700

    update

--- Algo + DS/scratch.js
+++ Algo + DS/scratch.js
@@ -1191,8 +1191,12 @@ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit int
 You must write an algorithm that runs in O(n) time and without using the division operation.
 */
 const productExceptSelf = (nums) => {
-	// First, create a prefix array that moves from the left,
-	// gathering the running product of the prefix at each index
+	// [1, 2, 3, 4] => [24, 12, 8, 6]
+	//          1, 1*1, 2*1*1, 3*2*1*1
+	// prefix: [1,  1,    2,     6]
+	//         2*3*4*1, 3*4*1, 4*1, 1
+	// suffix: [  24,     12,   4,  1]
+
 	const prefix = [];
 
 	// Move left in the input array
@@ -1206,25 +1210,20 @@ const productExceptSelf = (nums) => {
 			prefix[i] = nums[i - 1] * prefix[i - 1];
 		}
 	}
-	prefix
 
 	// Then, let's create a suffix array
 	const suffix = [];
 
 	// Move right in the input array
 	for (let i = nums.length - 1; i >= 0; i--) {
-		// For the last index, we have no suffix, so just add a 1 to that position
 		if (i === nums.length - 1) {
 			suffix[i] = 1;
 		} else {
-			// Otherwise, we multiply nums[i+1] by the suffix at position i+1
-			// and add that to the suffix array at position i
 			suffix[i] = nums[i + 1] * suffix[i + 1];
 		}
 	}
-	suffix
+	// suffix
 
-	// Finally, our result array should be the products of prefix * suffix for each position
 	const result = [];
 
 	for (let i = 0; i < nums.length; i++) {
@@ -1232,5 +1231,73 @@ const productExceptSelf = (nums) => {
 	}
 
 	return result;
+
+	// Optimized
+	/*
+	  // Set up an empty array as our result
+    const result = []
+    
+    // Initialize a prefix tracker at 1
+    let prefix = 1
+    
+    // Loop through the input array - for each position,
+    // the result array should equal the prefix tracker.
+    
+    // Then, update the prefix tracker to be the product of itself,
+    // multiplied by the input value at the position.
+    for (let i=0; i<nums.length; i++) {
+        result[i] = prefix
+        prefix *= nums[i]
+    }
+    
+    // Initialize a suffix tracker at 1
+    let suffix = 1
+    
+    // Loop backwards through the array.
+    // For each iteration, set the result array to be 
+    // the product of itself multiplied by the suffix tracker.
+    
+    // Then, update the suffix tracker to be the product of itself,
+    // multiplied by the input value at that position.
+    for (let i=nums.length - 1; i>=0; i--) {
+        result[i] *= suffix
+        suffix *= nums[i]
+    }
+
+    return result
+		*/
 };
 // console.log(productExceptSelf([1, 2, 3, 4]));
+
+/* 155. Min Stack
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+*/
+
+class MinStack {
+	constructor() {
+		this.stack = [];
+		this.min = Infinity;
+	}
+
+	// void push(int val) pushes the element val onto the stack.
+	push(val) {
+		this.stack.push({ val, prevMin: this.min });
+		this.min = Math.min(this.min, val);
+	}
+
+	//void pop() removes the element on the top of the stack.
+	pop() {
+		const deleted = this.stack.pop();
+		this.min = deleted.prevMin;
+	}
+
+	//int top() gets the top element of the stack.
+	top() {
+		return this.stack[this.stack.length - 1].val;
+	}
+
+	//int getMin() retrieves the minimum element in the stack.
+	getMin() {
+		return this.min;
+	}
+}
