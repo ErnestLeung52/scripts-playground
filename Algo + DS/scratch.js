d118707f6aa75d617194c2448188fd69d32333c6
Ernest Leung <ernestleung52@gmail.com>
2023-05-01 17:10:22 -0700

    update

--- Algo + DS/scratch.js
+++ Algo + DS/scratch.js
@@ -1648,4 +1648,21 @@ const merge = (intervals) => {
 };
 // console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
 
+/* 236. Lowest Common Ancestor of a Binary Tree
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+*/
+
+const lowestCommonAncestor = (root, p, q) => {
+	if (!root) return;
+
+	if (root === p || root === q) {
+		return root;
+	}
+
+	const left = lowestCommonAncestor(root.left, p, q);
+	const right = lowestCommonAncestor(root.right, p, q);
+
+	return left && right ? root : left || right;
+};
