7281e93a4a728a8c0f708a8687221e866f79b07b
Ernest Leung <ernestleung52@gmail.com>
2023-05-01 17:10:15 -0700

    update

--- Algo + DS/scratch.js
+++ Algo + DS/scratch.js
@@ -1619,3 +1619,33 @@ function permuteTemplate(nums) {
 	return results;
 }
 // console.log(permuteTemplate([1, 2, 3]));
+
+/*56. Merge Intervals
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+*/
+
+const merge = (intervals) => {
+	if (intervals.length === 0) return [[]];
+
+	intervals.sort((a, b) => a[0] - b[0]);
+
+	const result = [intervals[0]];
+
+	for (let i = 1; i < intervals.length; i++) {
+		const [start, end] = intervals[i];
+		const [prevStart, prevEnd] = result.at(-1);
+
+		if (start <= prevEnd) {
+			result.at(-1)[1] = Math.max(prevEnd, end);
+		} else {
+			result.push([start, end]);
+		}
+	}
+
+	return result;
+};
+// console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
+
+
