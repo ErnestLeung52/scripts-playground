25cc3afff9987c0df4f664d56446cfbdb4e406f3
Ernest Leung <ernestleung52@gmail.com>
2023-05-04 00:47:55 -0700

    update

--- Algo + DS/scratch.js
+++ Algo + DS/scratch.js
@@ -254,7 +254,7 @@ Given a binary search tree (BST), find the lowest common ancestor (LCA) node of 
 According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 */
 // O(logN )
-function lowestCommonAncestor(root, p, q) {
+function lowestCommonAncestor1(root, p, q) {
 	// top root is always the common ancestor but not the lowest
 	// p is less than curNode then it will be in the left subtree
 	// q is less than curNode then it will be in the right subtree
@@ -1666,3 +1666,46 @@ const lowestCommonAncestor = (root, p, q) => {
 
 	return left && right ? root : left || right;
 };
+
+/*981. Time Based Key-Value Store
+Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+TimeMap() Initializes the object of the data structure.
+void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+*/
+
+class TimeMap {
+	constructor() {
+		this.map = {};
+	}
+
+	set(key, value, timestamp) {
+		const keyVals = this.map[key] ? this.map[key] : [];
+		keyVals.push([value, timestamp]);
+		this.map[key] = keyVals;
+	}
+
+	get(key, timestamp) {
+		const valueTimestamp = this.map[key] ? this.map[key] : [];
+
+		let left = 0;
+		let right = valueTimestamp.length - 1;
+		let result;
+
+		while (left <= right) {
+			let mid = Math.floor((left + right) / 2);
+
+			if (valueTimestamp[mid][1] <= timestamp) {
+				result = valueTimestamp[mid][1];
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+
+		return result === null ? '' : result;
+	}
+}
