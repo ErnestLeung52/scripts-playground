8fd8e8d744b85359e8c491b9f8cb26a190c0bf4b
Ernest Leung <ernestleung52@gmail.com>
2023-08-21 22:56:33 -0700

    Stack Queue

---
+++ AceTheFrontEndInterview/DataStructure/StackQueue.js
@@ -0,0 +1,2 @@
+/* Implement a function findBin(n), which will generate binary numbers from 1 to n in the form of a string using a queue. An illustration is also provided for your understanding.
+ */
