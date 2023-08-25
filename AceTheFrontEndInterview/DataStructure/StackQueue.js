4fa9f8f2ce2bca526f4318516248ce995d029774
Ernest Leung <ernestleung52@gmail.com>
2023-08-24 22:07:32 -0700

    Stack Queue

--- AceTheFrontEndInterview/DataStructure/StackQueue.js
+++ AceTheFrontEndInterview/DataStructure/StackQueue.js
@@ -1,2 +1,5 @@
-/* Implement a function findBin(n), which will generate binary numbers from 1 to n in the form of a string using a queue. An illustration is also provided for your understanding.
+/* 1. Implement a function findBin(n), which will generate binary numbers from 1 to n in the form of a string using a queue. An illustration is also provided for your understanding.
+ */
+
+/* 2. You have to implement the minStack class, which will have a min() function. Whenever min() is called, the minimum value of the stack is returned in O(1) time. The element is not popped from the stack; its value is simply returned.
  */
