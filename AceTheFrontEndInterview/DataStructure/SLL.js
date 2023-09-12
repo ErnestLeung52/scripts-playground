a0a54611e1ec92cd54c9dc4ef8872072a9b1762e
Ernest Leung <ernestleung52@gmail.com>
2023-09-12 15:56:47 -0700

    Singly Linked Lists

---
+++ AceTheFrontEndInterview/DataStructure/SLL.js
@@ -0,0 +1,21 @@
+/*
+Singly Linked List DS:
+Head_{data, pointer} -> Node_{data1, pointer1} -> Node_{data2, pointer2} -> Null
+*/
+
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+class SinglyLinkedList {
+	constructor() {
+		this.head = null;
+	}
+
+	isEmpty() {
+		return this.head === null;
+	}
+}
