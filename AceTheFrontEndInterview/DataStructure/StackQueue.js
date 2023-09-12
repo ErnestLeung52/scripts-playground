e1761e0dfe7cb39eb97b373feefdf4d9c4eaa0f9
Ernest Leung <ernestleung52@gmail.com>
2023-09-12 12:06:20 -0700

    Stack Queue

--- AceTheFrontEndInterview/DataStructure/StackQueue.js
+++ AceTheFrontEndInterview/DataStructure/StackQueue.js
@@ -1,13 +1,57 @@
 /* 1. Implement a function findBin(n), which will generate binary numbers from 1 to n in the form of a string using a queue. An illustration is also provided for your understanding.
  */
 
-const findBin = () => {
+class Queue {
+	constructor() {
+		this.items = [];
+	}
 
+	isEmpty() {
+		return this.items.length === 0;
+	}
+
+	getFront() {
+		if (!this.isEmpty()) {
+			return this.items[0];
+		} else {
+			return null;
+		}
+	}
+
+	size() {
+		return this.items.length;
+	}
+
+	enqueue(el) {
+		return this.items.push(el);
+	}
+
+	dequeue() {
+		return this.items.shift();
+	}
+}
+
+const findBin = (n) => {
+	const result = [];
+	const queue = new Queue();
+
+	queue.enqueue('1');
+
+	for (let i = 0; i < n; i++) {
+		const currNum = queue.dequeue();
+
+		result.push(currNum);
+
+		queue.enqueue(currNum + '0');
+		queue.enqueue(currNum + '1');
+		queue;
+	}
+
+	return result;
 };
+// console.log(findBin(5));
 
 /* 2. You have to implement the minStack class, which will have a min() function. Whenever min() is called, the minimum value of the stack is returned in O(1) time. The element is not popped from the stack; its value is simply returned.
  */
 
-const findMin = () => {
-  
-};
+const findMin = () => {};
