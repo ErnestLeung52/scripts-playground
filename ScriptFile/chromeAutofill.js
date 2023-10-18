54ecc7512aeade2184f55c565156637d74b046d2
Ernest Leung <ernestleung52@gmail.com>
2023-10-18 14:07:23 -0700

    chrome update

--- ScriptFile/chromeAutofill.js
+++ ScriptFile/chromeAutofill.js
@@ -4,8 +4,11 @@ Object.assign(document.body.appendChild(document.createElement('input')), {
 	style: 'position:absolute; top:2ex; right:0; z-index:999',
 	onchange(e) {
 		if (!this.files[0]) return;
+
 		const fr = new FileReader();
+
 		fr.readAsText(this.files[0], 'UTF-8');
+
 		fr.onload = () => {
 			for (const line of fr.result.split(/\r?\n/)) {
 				const [email, fName, fN, lN, add, city, postal, state, phone, country] = line.split(',');
